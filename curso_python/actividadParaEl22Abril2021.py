5471b8992761b670175da150dd1279b8237202b6
pablo voss <pablo.voss.contractor@macmillan.com>
2021-04-26 22:00:39 -0300

    actividad para el 27 abril

--- curso_python/actividadParaEl22Abril2021.py
+++ curso_python/actividadParaEl22Abril2021.py
@@ -1,23 +1,23 @@
-nombre = input('Digame su nombre: ')
+cantidad_personas = int(input('Ingrese cuantas personas:'))
 
-apellido = input('Digame su apellido: ')
+for i in range(0, cantidad_personas):
+    nombre = input('Digame su nombre: ')
+    apellido = input('Digame su apellido: ')
+    edad = int(input('Digame su edad (debe estar entre 1 a 120 años)'))
+    while edad < 1 or edad > 120:
+        edad = int(input('Digame su edad (debe estar entre 1 a 120 años)'))
 
-edad = int(input('Digame su edad: '))
+    condicion = ''
+    if edad <0:
+        condicion = 'nonato'
+    elif edad <18:
+        condicion = 'menor'
+    elif edad <65:
+        condicion = 'mayor'
+    elif edad <120:
+        condicion = 'jubilado'
+    else:
+        condicion = 'cadaver'
 
-condicion = ''
-
-if edad <0:
-    condicion = 'nonato'
-elif edad <18:
-    condicion = 'menor'
-elif edad <65:
-    condicion = 'mayor'
-elif edad <120:
-    condicion = 'jubilado'
-else:
-    condicion = 'cadaver'
-
-frase = 'Su nombre es: ' + nombre + ' ' + apellido + ' y usted es ' + condicion
-print(frase)
-frase2 = ('Su nombre es {} {} y usted es {}'.format(nombre,apellido,condicion))
-print(frase2)
+    frase = ('Su nombre es {} {} y usted es {}'.format(nombre,apellido,condicion))
+    print(frase)
